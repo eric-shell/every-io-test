be39de607f4922d1a4cde6244c345de8da682a90
Eric Shell <ericjshell@gmail.com>
2025-01-27 09:57:13 -0800

    Adding the status labels, initial tasks and baseline markup

--- src/ChallengeComponent.tsx
+++ src/ChallengeComponent.tsx
@@ -1,10 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export function ChallengeComponent() {
+
+  const statusList = ['To Do', 'In Progress', 'Done'];
+  const initialTasks = [
+    { id: 1, title: 'Mow the Lawn', status: 'To Do' },
+    { id: 2, title: 'Pull Weeds', status: 'In Progress' },
+    { id: 3, title: 'Rake the Leaves', status: 'Done' }
+  ];
+
+  // Initialize future state varibles here
+
+  const handleAddTask = () => {
+    // Handle submitting new task to list state
+  }
+
+  const handleMoveTask = () => {
+    // Handle moving available tasks by updating the status
+  }
+
   return (
-    <>
-      {/* Delete this h2, and add your own code here. */}
-      <h2 style={{ textAlign: 'center', paddingTop: 200, paddingBottom: 200 }}>Your code goes here</h2>
-    </>
+    <div className="form">
+
+      <div className="form__columns">
+        <p>Column markup will go here.</p>
+      </div>
+
+      <div className="form__submit">
+        <input
+          className="form__input"
+          type="text"
+          placeholder="Enter new task"
+        />
+        <button
+          className="form__button"
+          onClick={handleAddTask}
+        >
+        Add
+        </button>
+      </div>
+
+    </div>
   )
 }
